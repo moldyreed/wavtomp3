7b13678d0a388277dcccfe9adaa0319b480a93fe
Aleksandr Matuzok <sherilynhope@gmail.com>
2016-12-11 17:29:16 +0300

    [IMPR] codestyle fix

--- src/nix/filesystem.cpp
+++ src/nix/filesystem.cpp
@@ -9,55 +9,52 @@
 
 std::vector<std::string> filesystem::getFilesByPath(const std::string& path, const std::string &filePattern)
 {
-	std::vector<std::string> files;
+    std::vector<std::string> files;
 
-	if (isDirectory(path))
-	{
-		char fileName[NAME_MAX] ;
-		struct dirent* dp;
-		DIR* dfd;
+    if (isDirectory(path)) {
+        char fileName[NAME_MAX] ;
+        struct dirent* dp;
+        DIR* dfd;
 
-		if ((dfd = opendir(path.c_str())) == NULL)
-		{
-			throw std::logic_error("Can't open directory: " + path);
-		}
+        if ((dfd = opendir(path.c_str())) == NULL) {
+            throw std::logic_error("Can't open directory: " + path);
+        }
 
-		while ((dp = readdir(dfd)) != NULL)
-		{
-			struct stat st {0};
-			sprintf(fileName , "%s/%s", path.c_str(), dp->d_name) ;
+        while ((dp = readdir(dfd)) != NULL) {
+            struct stat st {
+                0
+            };
+            sprintf(fileName , "%s/%s", path.c_str(), dp->d_name) ;
 
-			if (stat(fileName, &st) == -1)
-			{
-				std::cerr << "Unable to stat file: " << fileName << "\n";
-				continue ;
-			}
+            if (stat(fileName, &st) == -1) {
+                std::cerr << "Unable to stat file: " << fileName << "\n";
+                continue ;
+            }
 
-			if (S_ISDIR(st.st_mode))
-			{
-				continue;
-			}
-			else
-			{
-                if (fnmatch(filePattern.c_str(), dp->d_name, 0) == 0)
+            if (S_ISDIR(st.st_mode)) {
+                continue;
+            } else {
+                if (fnmatch(filePattern.c_str(), dp->d_name, 0) == 0) {
                     files.emplace_back(fileName);
-			}
-		}
-	}
-	else
-		throw std::logic_error("Not a directory");
+                }
+            }
+        }
+    } else {
+        throw std::logic_error("Not a directory");
+    }
 
-	return files;
+    return files;
 }
 
 bool filesystem::isDirectory(const std::string& path)
 {
-	struct stat pathStat {0};
+    struct stat pathStat {
+        0
+    };
 
-	if (stat(path.c_str(), &pathStat) != 0)
-	{
-		throw std::logic_error("Directory not exists");
-	}
+    if (stat(path.c_str(), &pathStat) != 0) {
+        throw std::logic_error("Directory not exists");
+    }
 
-	return S_ISDIR(pathStat.st_mode);
+    return S_ISDIR(pathStat.st_mode);
 }
--- src/win/filesystem.cpp
+++ src/win/filesystem.cpp
@@ -6,56 +6,50 @@
 
 std::vector<std::string> filesystem::getFilesByPath(const std::string& path, const std::string& filePattern)
 {
-	std::vector<std::string> files;
+    std::vector<std::string> files;
 
-        WIN32_FIND_DATA fdFile{0};
-        HANDLE hFind = NULL;
+    WIN32_FIND_DATA fdFile{0};
+    HANDLE hFind = NULL;
 
-        if(isDirectory(path))
-        {
-            auto newPath = std::string(path + "\\" + filePattern);
-            hFind = FindFirstFile(newPath.c_str(), &fdFile);
+    if(isDirectory(path)) {
+        auto newPath = std::string(path + "\\" + filePattern);
+        hFind = FindFirstFile(newPath.c_str(), &fdFile);
 
-               if (INVALID_HANDLE_VALUE == hFind)
-               {
-                  throw std::runtime_error("Something went wrong while path reading: " +  path);
-               }
-
-               do
-               {
-                  if (!(fdFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-                  {
-                     files.emplace_back(path + "\\" + fdFile.cFileName);
-                  }
-               }
-               while (FindNextFile(hFind, &fdFile) != 0);
-
-
-              auto dwError = GetLastError();
-               if (dwError != ERROR_NO_MORE_FILES)
-               {
-                   std::cout << dwError << std::endl;
-               }
-
-            FindClose(hFind);
-
-
+        if (INVALID_HANDLE_VALUE == hFind) {
+            throw std::runtime_error("Something went wrong while path reading: " +  path);
         }
-        else
-        {
-            throw std::runtime_error("Path not found: " +  path + " or it's not a directory.");
+
+        do {
+            if (!(fdFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+                files.emplace_back(path + "\\" + fdFile.cFileName);
+            }
+        } while (FindNextFile(hFind, &fdFile) != 0);
+
+
+        auto dwError = GetLastError();
+        if (dwError != ERROR_NO_MORE_FILES) {
+            std::cout << dwError << std::endl;
         }
-	return files;
+
+        FindClose(hFind);
+
+
+    } else {
+        throw std::runtime_error("Path not found: " +  path + " or it's not a directory.");
+    }
+    return files;
 }
 
 bool filesystem::isDirectory(const std::string& path)
 {
     DWORD ftyp = GetFileAttributesA(path.c_str());
-    if (ftyp == INVALID_FILE_ATTRIBUTES)
-      return false;
+    if (ftyp == INVALID_FILE_ATTRIBUTES) {
+        return false;
+    }
 
-    if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
-      return true;
+    if (ftyp & FILE_ATTRIBUTE_DIRECTORY) {
+        return true;
+    }
 
     return false;
 }
